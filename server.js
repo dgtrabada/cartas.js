5c06d6d2eb396a7bfcaa98c1397adc72fd3bccad
dgtrabada <dgtrabada@gmail.com>
2021-04-03 18:08:00 +0200

    repartir

--- server.js
+++ server.js
@@ -48,16 +48,14 @@ class Carta {
 }
 
 var carta=[]
-var ancho = 133
-var largo = 204
 Tx=Ty=800
 
 var id=0
 for (p=0;p<palo.length;p++){
   for (i=0;i<num.length;i++){
     // coincidencia cuatro jugadores y cuatro palos
-    aux_x=[200+40*i,80+p*120,Tx-(200+40*i),Tx-(80+p*120)]
-    aux_y=[80+p*120,200+40*i,Tx-(80+p*120),Tx-(200+40*i)]
+    aux_x=[Tx/2,Tx/2,Tx/2,Tx/2]
+    aux_y=[Ty/2,Ty/2,Ty/2,Ty/2]
     carta.push( new Carta(jugador[p],aux_x,aux_y,palo[p],num[i],false,"nadie",false,true) )
     id++;
   }
@@ -70,50 +68,6 @@ function print_cartas(){
   for(i=0;i<40;i++) console.log(carta[i].jugador.name+" "+carta[i].n+" "+carta[i].palo+" "+carta[i].tirada+" "+carta[i].visible);
 console.log("-------------")
 }
-
-function barajar(){
-  for(i=1;i<200;i++){
-    i1 = getRndInteger(0,40);
-    i2 = getRndInteger(0,40);
-    aux = carta[i2].jugador;
-    carta[i2].jugador = carta[i1].jugador;
-    carta[i1].jugador = aux;
-  }
-}
-
-
-//print_cartas();
-barajar();
-//print_cartas();
-
-
-  id=ip=ia=ig=5
-
-  for(i=0;i<40;i++) {
-    //console.log(carta[i].jugador.name)
-    if(carta[i].jugador.name=="dgtrabada"){
-      id++;
-      carta[i].x=[Tx/24*id,(Tx-ancho),Tx/24*id,ancho]
-      carta[i].y=[(Ty-largo),Ty/24*id,largo/2,Ty/24*id]
-      }
-    if(carta[i].jugador.name=="dguerra"){
-      ig++;
-      carta[i].x=[ancho,Tx/24*ig,(Tx-ancho),Tx/24*ig]
-      carta[i].y=[Ty/24*ig,(Ty-largo),Ty/24*ig,largo/2]
-      }
-    if(carta[i].jugador.name=="pangard"){
-     ip++;
-     carta[i].x=[Tx/24*ip,ancho,Tx/24*ip,(Tx-ancho)]
-     carta[i].y=[largo/2,Ty/24*ip,(Ty-largo),Ty/24*ip]
-     }
-   if(carta[i].jugador.name=="alsubias"){
-     ia++;
-     carta[i].x=[(Tx-ancho),25*ia,ancho,25*ia]
-     carta[i].y=[25*ia,largo/2,25*ia,(Ty-largo)]
-     }
-  }
-
-
 
 
 io.on('connection', function (socket) {
